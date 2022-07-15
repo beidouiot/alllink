eeb537f4260cb6ac4b4c98e0de638e1c72c73f32
3827291@qq.com <3827291@qq.com>
2022-07-15 15:39:05 +0800

    新增设备管理

--- common/common-data/data-entity/user-center-entity/src/main/java/com/beidouiot/alllink/community/common/data/entity/user/center/User.java
+++ common/common-data/data-entity/user-center-entity/src/main/java/com/beidouiot/alllink/community/common/data/entity/user/center/User.java
@@ -82,9 +82,6 @@ public class User extends BaseEntity implements UserDetails {
 	@Column(name = EntityConstants.DESCR, nullable = true, columnDefinition = EntityConstants.USER_DESCR_COLUMN_DEFINITION)
 	private String descr;
 	
-	@Column(name = EntityConstants.CODE, nullable = true, columnDefinition = EntityConstants.USER_CODE_COLUMN_DEFINITION)
-	private String code;
-	
 	@Column(name = EntityConstants.IDENTITY_NO, nullable = true, columnDefinition = EntityConstants.IDENTITY_NO_COLUMN_DEFINITION)
 	private String identityNo;
 	
@@ -93,15 +90,6 @@ public class User extends BaseEntity implements UserDetails {
 	
 	@Column(name = EntityConstants.TENANT_ID, nullable = true, columnDefinition = EntityConstants.TENANT_ID_COLUMN_DEFINITION)
 	private Long tenantId;
-	
-	@Column(name = EntityConstants.CUSTOMER_ID, nullable = true, columnDefinition = EntityConstants.CUSTOMER_ID_COLUMN_DEFINITION)
-	private Long customerId;
-	
-	@Column(name = EntityConstants.PARK_ID, nullable = true, columnDefinition = EntityConstants.PARK_ID_COLUMN_DEFINITION)
-	private Long parkId;
-	
-	@Column(name = EntityConstants.SYSTEM_CODE, nullable = true, columnDefinition = EntityConstants.SYSTEM_CODE_COLUMN_DEFINITION)
-	private String systemCode;
 
 	/**
      * 权限数据
