149189dcb774dd29f6124b286f7c08efafb0b72a
3827291@qq.com <3827291@qq.com>
2023-03-01 16:18:06 +0800

    增加设备数据采集调用

--- feign/product-feign/src/main/java/com/beidouiot/alllink/community/feign/product/ProductFeignClient.java
+++ feign/product-feign/src/main/java/com/beidouiot/alllink/community/feign/product/ProductFeignClient.java
@@ -3,15 +3,16 @@ package com.beidouiot.alllink.community.feign.product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.beidouiot.alllink.community.common.base.utils.ServiceConstants;
 import com.beidouiot.alllink.community.common.data.xxo.dto.ID;
 import com.beidouiot.alllink.community.common.data.xxo.product.dto.ProductDto;
 import com.beidouiot.alllink.community.common.data.xxo.rro.ResultDataRro;
 import com.beidouiot.alllink.community.feign.product.impl.ProductFeignClientFallback;
 
 
-@FeignClient(value = "product-center-server", fallback = ProductFeignClientFallback.class)
+@FeignClient(value = ServiceConstants.PRODUCT_CENTER_SERVER, fallback = ProductFeignClientFallback.class)
 public interface ProductFeignClient {
 	
-	@PostMapping("/pc/product/v1/findOne")
+	@PostMapping(ServiceConstants.PRODUCT_URI_BASE+"v1/findOne")
 	ResultDataRro<ProductDto> findProductById(ID id);
 }
--- feign/product-feign/src/main/java/com/beidouiot/alllink/community/feign/product/ProductModelFeignClient.java
+++ feign/product-feign/src/main/java/com/beidouiot/alllink/community/feign/product/ProductModelFeignClient.java
@@ -3,14 +3,15 @@ package com.beidouiot.alllink.community.feign.product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.beidouiot.alllink.community.common.base.utils.ServiceConstants;
 import com.beidouiot.alllink.community.common.data.xxo.dto.ID;
 import com.beidouiot.alllink.community.common.data.xxo.product.dto.ProductModelDto;
 import com.beidouiot.alllink.community.common.data.xxo.rro.ResultDataRro;
 import com.beidouiot.alllink.community.feign.product.impl.ProductModelFeignClientFallback;
 
-@FeignClient(value = "product-center-server", fallback = ProductModelFeignClientFallback.class)
+@FeignClient(value = ServiceConstants.PRODUCT_CENTER_SERVER, fallback = ProductModelFeignClientFallback.class)
 public interface ProductModelFeignClient {
 
-	@PostMapping("/pc/product/model/v1/findPubModel")
+	@PostMapping(ServiceConstants.PRODUCT_MODEL_URI_BASE+"v1/findPubModel")
 	ResultDataRro<ProductModelDto> findPubProductModelByProductId(ID id);
 }
