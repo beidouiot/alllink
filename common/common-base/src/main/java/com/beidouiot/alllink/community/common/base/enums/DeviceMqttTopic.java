28b19cadd42f6a2c341b2babc6e637145731a13c
3827291@qq.com <3827291@qq.com>
2023-05-10 09:42:22 +0800

    mqtt主题

--- common/common-base/src/main/java/com/beidouiot/alllink/community/common/base/enums/DeviceMqttTopic.java
+++ common/common-base/src/main/java/com/beidouiot/alllink/community/common/base/enums/DeviceMqttTopic.java
@@ -8,10 +8,7 @@ public enum DeviceMqttTopic {
 	EVENT_DOWN("${productName}/${deviceName}/events/down","事件下发"),
 	COMMANDS_UP("${productName}/${deviceName}/commands/up","指令上报"),
 	COMMANDS_DOWN("${productName}/${deviceName}/commands/down","指令下发");
-	
-	
-	
-	
+
 	
     private DeviceMqttTopic(String topic, String remark) {
 		this.topic = topic;
